85103543227ef94c080bfee2d0f6594ff64cb0e3
Cynthia <cynthia.decelle@gmail.com>
2022-04-13 21:12:52 +0200

    Tutorial version https://www.youtube.com/watch?v=E1E08i2UJGI

--- src/App.js
+++ src/App.js
@@ -1,77 +1,13 @@
-import React, { useState } from "react";
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
 
-//presentational component which takes the item as a prop and display it
-
-//component that will contain a list of all the items that we have in the app. Takes items as a prop and display them as ul.
-// Uses Item component from above for displaying individual elements
-
-
-// Hooked them up in the App component and pass the items as props to the ItemList
-
-// Having the items in a state variable and setter function that will be used to update these items
-const App = () => {
-  const [items, setItems] = useState([]);
-  
-  
-  const Item = ({item, removeItem}) =>
-  {
-    return (
-      <div>
-        <span>{item}</span>
-        <buton button onclick={() => removeItem(item)}>X</buton>
-     </div>
-    );
-  };
-
-  const ItemList = ({items}) => {
-    return (
-      <div className="items-container">
-        <ul>
-          {items.map((item) => (
-          <li>
-            <Item key={item} item={item}/>
-          </li>))}
-        </ul>
-      </div>
-    );
-  };
-  const addItem = (item) => {
-      setItems([...items, item]);
-    };    
-  const AddItemForm = ({addItem}) => {
-    const [item, setItem] = useState('');
-  
-    const handleSubmit = (e) => {
-      e.prevent.Default();
-      addItem(item);
-      setItem('');
-    };
-    return (
-      <div>
-        <p>Add item</p>
-        <form onSubmit={handleSubmit}>
-          <input value={item} onChange={ (e) => setItem(e.target.value)} />
-          <button>Add Item</button>
-        </form>
-      </div>
-    );
-  }
-  
-
-  const removeItem = (itemToBeDeleted) => {
-    setItems(items.filter((item) => itemToBeDeleted !== item));
-  };
-
-  
-  return(
-    <div className="App">
-      <header className="App-header">
-        To Do items
-        < AddItemForm items={items} />
-      </header>
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
     </div>
   );
-
-};
+}
 
 export default App;
