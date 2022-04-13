f8ffef57564b5f037f629be2dd530a13060c344e
Cynthia <cynthia.decelle@gmail.com>
2022-04-13 12:04:00 +0200

    Start ex

--- src/App.js
+++ src/App.js
@@ -1,63 +1,48 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Item from './Item';
+import ItemList from './ItemList';
 
-// component
-function Todo({ todo }) {
+//presentational component which takes the item as a prop and display it
+const Item = ({item}) =>
+{
   return (
-    <div className="todo">
-      {todo.text}
+    <div>
+      <span>{item}</span>
     </div>
   );
 };
 
-//component
-function TodoForm({ addTodo }) {
-  const [value, setValue] = React.useState("");
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
-    setValue("");
-  };
-
+//component that will contain a list of all the items that we have in the app. Takes items as a prop and display them as ul.
+// Uses Item component from above for displaying individual elements
+const ItemList = ({items}) => {
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="input"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-    </form>
-  );
-}
-
-//component
-function App() { //state
-  const [todos, setTodos] = React.useState([ //name the state, set that state
-    { text: "Learn about React" },
-    { text: "Meet friend for lunch" },
-    { text: "Build really cool todo app" }
-  ]);
-    const addTodo = text => { // function will grab the items, add on the new item, and display that new list
-      const newTodos = [...todos, { text }];
-      setTodos(newTodos);
-    };
-  return ( //adds a <div> for app, a <div> for todo-list and a map of the todos to Todo components
-    <div className="app">
-      <div className="todo-list">
-        {todos.map((todo, index) => ( //map method create new array of items by mapping over the todo items from state and displaying them by index
-          <Todo
-            key={index}
-            index={index}
-            todo={todo}
-          />
-        ))}
-        <TodoForm addTodo={addTodo} />
-      </div>
+    <div className="items-container">
+      <ul>
+        {items.map((item) => (
+        <li>
+          <Item key={item} item={item}/>
+        </li>))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+// Hooked them up in the App component and pass the items as props to the ItemList
+
+const App = () => {
+  const items = ["item 1", "item 2"]
+  return(
+    <div className="App">
+      <header className="App-header">
+        To Do items
+        <ItemList items={items} />
+      </header>
+    </div>
+  );
+};
+
+
+
+
+
+export {Item as default};
