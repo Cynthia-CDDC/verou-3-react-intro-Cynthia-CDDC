450b5ad6f156e40b54381cee081dd4302c78552d
Cynthia <cynthia.decelle@gmail.com>
2022-04-19 16:28:02 +0200

    Update key declaration on list item instead of input checkbox

--- third-attempt/src/TodoList.js
+++ third-attempt/src/TodoList.js
@@ -2,15 +2,15 @@ import React, {useState} from "react";
 
 export default function TodoList() {
     const [todos, setTodos] = useState([
-        {id: 1, content: "My first todo",}, 
-        {id: 2, content: "My second todo",}
+        {id: 1, content: "My first todo", checked: false}, 
+        {id: 2, content: "My second todo", checked: false}
     ]);
-
+    // id: todos.length + 1
     return (
         <ul>
         {todos.map((todo) => (
-            <li>
-                <input type="checkbox" key={todo.id} /> {todo.content}
+            <li key={todo.id}>
+                <input type="checkbox" /> {todo.content}
             </li>
         ))}
         </ul>
